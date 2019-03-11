a008b0eb5de08c15cb9f03fbca421ea24c376b41
1996-10-09 <oshadee2kaushalya@gmail.com>
2019-03-11 08:57:04 +0530

    make countdown timer

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -8,12 +8,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private int counter = 99;
+private boolean running;
+private boolean wasrunning;
+private static final String CURRENT_COUNTER = "counter";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = savedInstanceState.getInt(CURRENT_COUNTER);
         countdown();
     }
+
+      @Override
+
+      public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,counter);
+      }
+
     private void countdown() {
         final TextView textView = findViewById(R.id.textView);
          final Handler handler = new Handler();
